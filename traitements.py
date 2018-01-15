7a4727c949f4c01cc16447f5c64937b61bdf1344
Kanamahk <tristan.hermant@u-psud.fr>
2018-01-15 18:37:54 +0100

    exemple traitements basique

--- traitements.py
+++ traitements.py
@@ -4,4 +4,11 @@ import re
 import collections
 
 def faireTraitement(data):
-	;
+	data = basicTraitement(data)
+
+	return data
+	
+def basicTraitement(data):
+	for line in data:
+		line = line.lower()
+	return data;
