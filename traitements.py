6b349544da299ea679d2364ccc5c0d8ba0f09243
Kabolas <suri.mur.bry@gmail.com>
2018-01-15 19:24:58 +0100

    traitement de la ponctuation, pour les ,.;:!?

--- traitements.py
+++ traitements.py
@@ -1,7 +1,14 @@
 #-*-coding:utf-8-*-
-
 import re 
 import collections
 
 def faireTraitement(data):
-	;
+    i = 0
+    ret = ""
+    while i < len(data):
+        if(data[i] != '.' or data[i] != ',' or data[i] != ';' or data[i] != '!' or data[i] != '?' or data[i] != ':'):
+            ret += data[i]
+        else:
+            i = i + 1
+        i = i + 1
+    return ret
